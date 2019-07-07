c2eaa7d753eac10db4c022a5e853abf1e3b74c4b
juaniviola <juaniviola1297@gmail.com>
2019-07-06 23:45:16 -0300

    express app

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const http = require('http')
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' })
-  res.end('Hello world')
-})
+'use strict'
 
-server.listen(8000)
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const authUsers = require('./auth/index')
+require('dotenv').config()
+
+const config = {
+  privateKey: process.env.PRIVATE_KEY,
+  mongoUrl: process.env.MONGO_URL,
+  port: process.env.PORT
+}
+
+mongoose
+  .connect(config.mongoUrl, { useNewUrlParser: true })
+  .then(() => console.log('Connected to database'))
+  .catch(err => console.error(`[ERROR]: ${err}`))
+
+app.use(express.json())
+app.use('api/users', authUsers.routerApi)
+
+app.listen(config.port, () => console.log(`[App] listening on port ${config.port}`))
